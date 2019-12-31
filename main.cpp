5ee71cb9ced581fd5beeb3eec7200dfe9e3f4903
azkahariz <azkahariz@gmail.com>
2020-01-01 00:04:42 +0700

    commit

--- main.cpp
+++ main.cpp
@@ -5,22 +5,36 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
 
+using namespace std;
+using namespace cv;
+int main()
+{
+    Mat img, gray, img_blur;
+    VideoCapture cap;
+    cap.open(1);
+    namedWindow("Image",WINDOW_KEEPRATIO);
+    for(;;)
+    {
+        cap >> img;
+        // Read the image as gray-scale
+        //img = imread("circles.png", IMREAD_COLOR);
+        // Convert to gray-scale
+        cvtColor(img, gray, CV_BGR2GRAY);
+        // Blur the image to reduce noise
+        medianBlur(gray, img_blur, 3);
+        // Create a vector for detected circles
+        vector<Vec3f>  circles;
+        // Apply Hough Transform
+        HoughCircles(img_blur, circles, HOUGH_GRADIENT, 1, img.rows/8, 200, 50, 200, 270);
+        // Draw detected circles
+        cout << circles.size() << endl;
+        for(size_t i=0; i<circles.size(); i++) {
+            Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+            int radius = cvRound(circles[i][2]);
+            circle(img, center, radius, Scalar(255, 255, 255), 2, 8, 0);
+        }
+        imshow("Image",img);
+        waitKey(33);
+    }
 
-
-// Read the image as gray-scale
-img = imread("circles.png", IMREAD_COLOR);
-// Convert to gray-scale
-gray = cvtColor(img, COLOR_BGR2GRAY);
-// Blur the image to reduce noise
-Mat img_blur;
-medianBlur(gray, img_blur, 5);
-// Create a vector for detected circles
-vector<Vec3f>  circles;
-// Apply Hough Transform
-HoughCircles(img_blur, circles, HOUGH_GRADIENT, 1, img.rows/64, 200, 10, 5, 30);
-// Draw detected circles
-for(size_t i=0; i<circles.size(); i++) {
-    Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-    int radius = cvRound(circles[i][2]);
-    circle(img, center, radius, Scalar(255, 255, 255), 2, 8, 0);
 }
